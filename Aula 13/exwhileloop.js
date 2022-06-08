0fc357813e32820d4d3fcf02db221060de747983
Layan <106406329+LayanOliveira@users.noreply.github.com>
2022-06-08 01:20:27 -0300

    Add files via upload

---
+++ Aula 13/exwhileloop.js
@@ -0,0 +1,14 @@
+console.log("Contador 'Do While', executa depois testa")
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+console.log("Contador 'While', testa depois executa")
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+} 
